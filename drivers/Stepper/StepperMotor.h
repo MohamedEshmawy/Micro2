7c97145d2ceee4da5d50d93898fd836ac81a81b5
MohamedEshmawy <36339453+MohamedEshmawy@users.noreply.github.com>
2019-04-26 13:14:36 +0200

    Added driver module to control the stepper motor using the H bridge driver

---
+++ drivers/Stepper/StepperMotor.h
@@ -0,0 +1,12 @@
+
+#include "../MCAL/MCAL.h"
+#include "IC_L293D.h"
+
+typedef struct
+{
+	IC_L293D ic_L293D;
+	int gearToothNum;
+} StepperMotor;
+
+StepperMotor initMotor(IC_L293D ic_L293D ,int gearToothNum);
+void rotateDegrees(StepperMotor m, int degrees, _Bool direction);
