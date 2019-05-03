6a055e814947db51ea6846dd806bc90d984c976f
yara-alaa <36486268+yara-alaa@users.noreply.github.com>
2019-05-03 04:15:29 +0200

    my contribution only switches code

---
+++ Switch1.h
@@ -0,0 +1,8 @@
+#include "tm4c123gh6pm.h"
+
+void SW1_Init(void);
+void SW2_Init(void);
+unsigned char SW1_Input(void);
+unsigned char SW2_Input(void);
+void read_send0(void);
+void read_send2(void);
