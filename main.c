eefc03c1394b4e67250a642e9c36e166f8e7d7cd
EmanElkot <21238429+EmanElkot@users.noreply.github.com>
2019-05-15 00:24:31 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
-#include "PWM.h"
+#include "Switch.h"
+#include "UartInit.h"
+#define GPIO_PCTL_PF321_M		(GPIO_PCTL_PF3_M | GPIO_PCTL_PF2_M | GPIO_PCTL_PF1_M)
+
+
 void SystemInit(){
-	Uart0_Initt();
-	pwm_init();
+	//Uart0_Initt();       //this was for testing
+	Uart1_Initt();
+	SW2_Init();
+	SW1_Init();
+
 }
 int main(void)
 {		 
-	  
-
 		while(1)
 		{
-			LED();        //to switch the led with signal of Pot of other Tiva 
-			//PWM1_3_CMPA_R = (15999-((UART0_getChar()-48)/9.0)*15999); this is to be on UART0 for testing 
+			read_send1();      //this function sends and receives on UART1
+			//read_send0();      //this function sends and receives on UART0
 		}
-} 
-
+} 	
